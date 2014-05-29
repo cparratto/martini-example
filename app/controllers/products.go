792e0553d26e2751beea5ce217301a115a6f4c98
Ignacio Galindo <joiggama@gmail.com>
2014-05-29 12:09:52 -0500

    Hood: Products#index

--- app/controllers/products.go
+++ app/controllers/products.go
@@ -4,93 +4,87 @@ import (
 	"net/http"
 	"strconv"
 
-	"github.com/coopernurse/gorp"
+	"github.com/eaigner/hood"
 	"github.com/go-martini/martini"
 	"github.com/martini-contrib/render"
 
 	"github.com/joiggama/martini-example/app/models"
 )
 
-func ProductsIndex(db *gorp.DbMap, params martini.Params, render render.Render, request *http.Request) {
+func ProductsIndex(db *hood.Hood, params martini.Params, render render.Render, request *http.Request) {
 	query := request.URL.Query()
 
-	var limit, offset string
+  limit, _  := strconv.Atoi(query.Get("limit"))
+  offset, _ := strconv.Atoi(query.Get("offset"))
 
-	//// There are no ORM chain methods for querying
-	if query.Get("limit") != "" {
-		limit = " LIMIT " + query.Get("limit")
-	}
-	if query.Get("offset") != "" {
-		offset = " OFFSET " + query.Get("offset")
-	}
 
-	var products []models.Product
-	_, err := db.Select(&products, "SELECT * FROM products"+limit+offset)
+	var products []models.Products
+	err := db.Limit(limit).Offset(offset).Find(&products)
 
 	if err == nil {
 		render.JSON(200, products)
 
 	} else {
-		render.JSON(404, "")
+		render.JSON(404, err.Error())
 	}
 
 }
 
-func ProductsShow(db *gorp.DbMap, params martini.Params, render render.Render) {
-	id, err := strconv.Atoi(params["id"])
-	if err != nil {
-		panic(err)
-	}
-	product, err := db.Get(models.Product{}, id)
+//func ProductsShow(db *hood.Hood, params martini.Params, render render.Render) {
+	//id, err := strconv.Atoi(params["id"])
+	//if err != nil {
+		//panic(err)
+	//}
+	//product, err := db.Get(models.Product{}, id)
 
-	if err == nil {
-		render.JSON(200, product)
-	} else {
-		render.JSON(404, map[string]string{"error": err.Error()})
-	}
-}
+	//if err == nil {
+		//render.JSON(200, product)
+	//} else {
+		//render.JSON(404, map[string]string{"error": err.Error()})
+	//}
+//}
 
-func ProductsCreate(product models.Product, db *gorp.DbMap, render render.Render) {
-	err := db.Insert(&product)
-	if err == nil {
-		render.JSON(201, &product)
-	} else {
-		render.JSON(422, map[string]string{"error": err.Error()})
-	}
-}
+//func ProductsCreate(product models.Product, db *hood.Hood, render render.Render) {
+	//err := db.Insert(&product)
+	//if err == nil {
+		//render.JSON(201, &product)
+	//} else {
+		//render.JSON(422, map[string]string{"error": err.Error()})
+	//}
+//}
 
-// Currently there's no way to only update specific columns, update maps struct fields to table :(
-// https://github.com/coopernurse/gorp/issues/92
-func ProductsUpdate(product models.Product, db *gorp.DbMap, params martini.Params, render render.Render) {
-	id, _ := strconv.ParseInt(params["id"], 0, 64)
-	product.Id = id
-	_, err := db.Update(&product)
+//// Currently there's no way to only update specific columns, update maps struct fields to table :(
+//// https://github.com/coopernurse/gorp/issues/92
+//func ProductsUpdate(product models.Product, db *hood.Hood, params martini.Params, render render.Render) {
+	//id, _ := strconv.ParseInt(params["id"], 0, 64)
+	//product.Id = id
+	//_, err := db.Update(&product)
 
-	if err == nil {
-		render.JSON(200, product)
-	} else {
-		render.JSON(422, err.Error())
-	}
-}
+	//if err == nil {
+		//render.JSON(200, product)
+	//} else {
+		//render.JSON(422, err.Error())
+	//}
+//}
 
-func ProductsDelete(db *gorp.DbMap, params martini.Params, render render.Render) {
-	id, _ := strconv.ParseInt(params["id"], 0, 64)
-	_, err := db.Delete(&models.Product{Id: id})
+//func ProductsDelete(db *hood.Hood, params martini.Params, render render.Render) {
+	//id, _ := strconv.ParseInt(params["id"], 0, 64)
+	//_, err := db.Delete(&models.Product{Id: id})
 
-	if err == nil {
-		render.JSON(204, "No content")
-	} else {
-		render.JSON(404, "Not found") // Let's just say that's the reason
-	}
-}
+	//if err == nil {
+		//render.JSON(204, "No content")
+	//} else {
+		//render.JSON(404, "Not found") // Let's just say that's the reason
+	//}
+//}
 
-func ProductsBulkCreate(products models.Products, db *gorp.DbMap, render render.Render) {
-	for _, product := range products.Collection {
-		err := db.Insert(&product)
-		if err != nil {
-			panic(err)
-		}
-	}
+//func ProductsBulkCreate(products models.Products, db *hood.Hood, render render.Render) {
+	//for _, product := range products.Collection {
+		//err := db.Insert(&product)
+		//if err != nil {
+			//panic(err)
+		//}
+	//}
 
-	render.JSON(201, products)
-}
+	//render.JSON(201, products)
+//}
--- config/db.go
+++ config/db.go
@@ -1,35 +1,33 @@
 package config
 
 import (
-	"database/sql"
-	"log"
+	//"database/sql"
+	//"log"
 	"os"
 
-	"github.com/coopernurse/gorp"
+	"github.com/eaigner/hood"
 	_ "github.com/lib/pq"
 
-	"github.com/joiggama/martini-example/app/models"
+	//"github.com/joiggama/martini-example/app/models"
 )
 
-func DB() *gorp.DbMap {
-	connection, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+func DB() *hood.Hood {
+	db, err := hood.Open("postgres", os.Getenv("DATABASE_URL"))
 
 	if err != nil {
 		panic(err)
 	}
 
-	db := &gorp.DbMap{Db: connection, Dialect: gorp.PostgresDialect{}}
-
-	registerTables(db)
-	enableLogging(db)
+	//registerTables(db)
+	//enableLogging(db)
 
 	return db
 }
 
-func enableLogging(db *gorp.DbMap) {
-	db.TraceOn("[gorp]", log.New(os.Stdout, "", log.Lmicroseconds))
-}
+//func enableLogging(db *gorp.DbMap) {
+	//db.TraceOn("[gorp]", log.New(os.Stdout, "", log.Lmicroseconds))
+//}
 
-func registerTables(db *gorp.DbMap) {
-	db.AddTableWithName(models.Product{}, "products").SetKeys(true, "Id")
-}
+//func registerTables(db *gorp.DbMap) {
+	//db.AddTableWithName(models.Product{}, "products").SetKeys(true, "Id")
+//}
